befdaa7cb2f73673e04b181a53b61259d43d4988
Luv Patel <luvp98@gmail.com>
2026-02-15 21:05:23 -0500

    Publish to Github

---
+++ cc_4.js
@@ -0,0 +1,39 @@
+// Array for all product information
+
+let products = [
+
+{
+    Name: "Soccer Ball",
+    category: "Sports",
+    price: 25,
+    inventory: 54 
+},
+{   Name: "Football",
+    category: "Sports",
+    price: 20,
+    inventory: 24 
+},
+{   Name: "Basketball",
+    category: "Sports",
+    price: 23,
+    inventory: 102 
+},
+{   Name: "Golf Ball",
+    category: "Sports",
+    price: 20,
+    inventory: 56 
+},
+{   Name: "Volleyball",
+    category: "Sports",
+    price: 15,
+    inventory: 76 
+}
+];
+
+// For loop to apply dynamic discount
+
+for (let product of products) {
+    let discountRate = 0;
+    
+    switch 
+}
